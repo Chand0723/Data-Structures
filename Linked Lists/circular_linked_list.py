6ad29cb0eedc707634359701d85b107639404aad
Chand Pasha <chandpasha0723@gmail.com>
2021-03-31 23:38:14 +0530

    updated CLL

--- Linked Lists/circular_linked_list.py
+++ Linked Lists/circular_linked_list.py
@@ -1,5 +1,3 @@
-## unfinshed implementation
-
 class Node:
     def __init__(self, data):
         self.data = data
@@ -8,45 +6,82 @@ class Node:
 class CircularLinkedList:
     def __init__(self):
         self.head = None
+        self.tail = None
         self.length = 0
 
     def insert_at_beginning(self, data):
         new_node = Node(data)
         if self.length == 0:
-            self.head = new_node
-            new_node.next = new_node
-        else:
-            current = self.head
-            while current.next != self.head:
-                current = current.next
+            self.head = self.tail = new_node
             new_node.next = self.head
-            current.next = new_node
+        else:
+            new_node.next = self.head
             self.head = new_node
+            self.tail.next = new_node
         self.length += 1
 
     def insert_at_end(self, data):
         new_node = Node(data)
-        current = self.head
-        while current.next != self.head:
-            current = current.next
-        new_node.next = self.head
-        current.next = new_node
+        if self.length == 0:
+            self.head = self.tail = new_node
+            new_node.next = self.head
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+            new_node.next = self.head
         self.length += 1
 
     def print_list(self):
-        node_list = []
-        current = self.head
-        while current.next != self.head:
-            node_list.append(current.data)
-        print(node_list)
+        list_node = []
+        if self.length == 0:
+            print('List is Empty')
+        else:
+            current = self.head
+            list_node.append(current.data)
+            while current.next != self.head:
+                current = current.next
+                list_node.append(current.data)
+            print(list_node)
+
+    def delete_from_front(self):
+        if self.length == 0:
+            print('List is Empty')
+        else:
+            if self.head != self.tail:
+                self.head = self.head.next
+                self.tail.next = self.head
+                self.length -= 1
+            else:
+                self.head = self.tail = None
+                self.length -= 1
+        
+    def delete_from_last(self):
+        if self.length == 0:
+            print('List is empty')
+        else:
+            if self.head != self.tail:
+                current = self.head
+                while current.next != self.tail:
+                    current = current.next
+                self.tail = current
+                self.tail.next = self.head
+                self.length -= 1
+            else:
+                self.head = self.tail = None
+                self.length -= 1
 
     
+    
 cll = CircularLinkedList()
-cll.insert_at_beginning(1)
 cll.insert_at_beginning(2)
+cll.insert_at_beginning(1)
+cll.insert_at_end(3)
+cll.insert_at_end(4)
+cll.insert_at_end(5)
 
-#cll.insert_at_end(2)
-#cll.insert_at_end(3)
+cll.print_list()
 
+cll.delete_from_front()
+cll.delete_from_last()
 cll.print_list()
 print(cll.length)
